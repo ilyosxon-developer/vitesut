ab2f6dcfafab6df9f241313e6d1efb75ff3263cd
Ismingiz <forfunzero123qaley111@gmail.com>
2025-02-13 16:42:46 +0500

    O'zgartirishlar qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 }
 
 export default App;
-// salom
+// eng oxirgisi
 // import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { RecoilRoot } from "recoil";
