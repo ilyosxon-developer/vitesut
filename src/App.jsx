b8927c2a15e7fde99d7deb1059a7cd93e336b221
Ismingiz <forfunzero123qaley111@gmail.com>
2025-02-13 15:30:52 +0500

    O'zgartirish tavsifi bu yerda

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
 }
 
 export default App;
+// salom
 // import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { RecoilRoot } from "recoil";
